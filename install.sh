aa7623fedd3db0388d128570fc4c99a3afaa0cac
Orv <70858122+ItsOrv@users.noreply.github.com>
2024-09-10 15:50:03 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,30 @@
-kk
+#!/bin/bash
+
+# Clone the project repository
+git clone https://github.com/ItsOrv/Telegram-Message-Monitor.git
+cd Telegram-Message-Monitor
+
+# Create the .env file if it doesn't exist
+if [ ! -f .env ]; then
+    echo "Creating .env file..."
+
+    # Ask user for input and write to .env file
+    read -p "Enter your API_ID: " api_id
+    read -p "Enter your API_HASH: " api_hash
+    read -p "Enter your BOT_TOKEN: " bot_token
+    read -p "Enter your SESSION_NAME(anything): " session_name
+
+    echo "API_ID=$api_id" > .env
+    echo "API_HASH=$api_hash" >> .env
+    echo "BOT_TOKEN=$bot_token" >> .env
+    echo "SESSION_NAME=$session_name" >> .env
+
+    echo ".env file created successfully."
+else
+    echo ".env file already exists."
+fi
+
+# Build and run the Docker container
+docker-compose up --build -d
+
+echo "Project setup complete and bot is running."
